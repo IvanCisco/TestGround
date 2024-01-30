493a30115ad85da3481577314951a8e47ae92661
ilMarra <158060631+ilMarra@users.noreply.github.com>
2024-01-30 16:40:35 +0100

    mnodifica css

--- ProvaConsegna/login.php
+++ ProvaConsegna/login.php
@@ -17,35 +17,38 @@ session_start();
         include("backend/logincheck.php");
         ?>
         <h1>Benvenuto</h1>
-        <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-		<!--<form method="post" action"/check/your_script.php">-->
-		
-            <label for="email"><b>E-mail</b></label>
-            <input type="email" name="mail" maxlength="40" required>
-            <span class="error"><?php echo $mailErr;?></span>
-            <br><br>
-            <label for="password"><b>Password</b></label>
-            <input type="password" name="password" maxlength="30" required>
-            <span class="error"><?php echo $passwordErr;?></span>
-            <br><br>
-            <label for="utente">Utente</label>
-            <select name="utente" required>
-                <option disabled selected value></option>
-                <option value="acquirente">Acquirente</option>
-                <option value="fattorino">Fattorino</option>
-                <option value="ristorante">Ristorante</option>
-            </select>
-            <br><br>
-            <button type="submit" name="login">Login</button>
-            <span class="error"><?php echo $loginErr;?></span>
-            <div>
-                <span><a href="backend/registrazione_acq.php">Crea account acquirente</a></span>
+        <br>
+        <div class=login-form>
+            <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+    		<!--<form method="post" action"/check/your_script.php">-->
+    		
+                <label for="email"><b>E-mail</b></label>
+                <input type="email" name="mail" maxlength="40" required>
+                <span class="error"><?php echo $mailErr;?></span>
                 <br><br>
-                <span><a href="backend/registrazione_rist.php">Crea account ristorante</a></span>
+                <label for="password"><b>Password</b></label>
+                <input type="password" name="password" maxlength="30" required>
+                <span class="error"><?php echo $passwordErr;?></span>
                 <br><br>
-                <span><a href="backend/registrazione_fatt.php">Crea account fattorino</a></span>
-            </div>
-        </form>
+                <label for="utente">Utente</label>
+                <select name="utente" required>
+                    <option disabled selected value></option>
+                    <option value="acquirente">Acquirente</option>
+                    <option value="fattorino">Fattorino</option>
+                    <option value="ristorante">Ristorante</option>
+                </select>
+                <br><br>
+                <button type="submit" name="login">Login</button>
+                <span class="error"><?php echo $loginErr;?></span>
+                <div>
+                    <span><a href="backend/registrazione_acq.php">Crea account acquirente</a></span>
+                    <br><br>
+                    <span><a href="backend/registrazione_rist.php">Crea account ristorante</a></span>
+                    <br><br>
+                    <span><a href="backend/registrazione_fatt.php">Crea account fattorino</a></span>
+                </div>
+            </form>
+    </div>
 
         <!-- Include the footer -->
     <?php include("common/footer.html"); ?>
