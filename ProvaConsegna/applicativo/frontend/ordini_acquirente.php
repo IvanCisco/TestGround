fa0779e0cd3cdde72aa398d0dfd87e2a17a1c112
ilMarra <158060631+ilMarra@users.noreply.github.com>
2024-02-02 16:28:07 +0100

    css e robe

--- ProvaConsegna/applicativo/frontend/ordini_acquirente.php
+++ ProvaConsegna/applicativo/frontend/ordini_acquirente.php
@@ -14,14 +14,18 @@
     <div class="header">
     <div class="navbar">
         <li><img src="../images/MainIcon.png" height="40px"></li>
-        <a href="acquirente.php">Torna indietro</a>
+        <!--<a href="acquirente.php">Torna indietro</a>-->
+       
         <a href="../frontend/profilo_acquirente.php">Profilo</a>
         <a href="../frontend/modificaprofilo_acquirente.php">Modifica Profilo</a>
         <a href="../common/logout.php">Logout</a>
     </div>
     </div>
 
-<h1>I tuoi ordini</h1>
+    <div class="titolo">
+         <a href="acquirente.php" class="indietro"><img src="../immagini/back.png" alt="Freccia indietro" class="freccia"></a>
+        <h1>I tuoi ordini</h1>
+    </div>
 <section class="page-content">
 <?php
 include("../common/connessione.php");
@@ -30,8 +34,8 @@ if (isset($_SESSION['utente'])) {
     $mail = $_SESSION['utente'];
 
 
-$stmt = $conn->prepare( "SELECT data,ora,stato,metodopagamento FROM ordine WHERE mailacq	 = ?  ORDER BY data DESC,ora DESC");
-	$stmt->bind_param("s", $mail);
+$stmt = $conn->prepare( "SELECT data,ora,stato,metodopagamento FROM ordine WHERE mailacq     = ?  ORDER BY data DESC,ora DESC");
+    $stmt->bind_param("s", $mail);
     $stmt->execute();
     $result = $stmt->get_result();
 
@@ -42,7 +46,7 @@ $stmt = $conn->prepare( "SELECT data,ora,stato,metodopagamento FROM ordine WHERE
     echo "<h2>I tuoi ordini :</h2>";
             echo "<ul>";*/
     
-	// per ogni riga
+    // per ogni riga
     while ($row = $result->fetch_assoc()) {
 
 
