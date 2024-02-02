d2c29cdd2c9f3c64a761fb699e7ff416f56b195c
ilMarra <158060631+ilMarra@users.noreply.github.com>
2024-02-02 14:35:19 +0100

    Update modificaprofilo_acquirente.php
    avevo lasciato la stessa pagina

--- ProvaConsegna/frontend/modificaprofilo_acquirente.php
+++ ProvaConsegna/frontend/modificaprofilo_acquirente.php
@@ -25,7 +25,6 @@
         <a href="acquirente.php">Torna indietro</a></p>
         <a href="../frontend/ordini_acquirente.php">Ordini</a>
         <a href="../frontend/profilo_acquirente.php">Profilo</a>
-        <a href="../frontend/modificaprofilo_acquirente.php">Modifica Profilo</a>
         <a href="../common/logout.php">Logout</a>
     </div>
 
