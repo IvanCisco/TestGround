32b7d669fd8208b04ae3495eec73d1896ed79420
ilMarra <158060631+ilMarra@users.noreply.github.com>
2024-02-08 00:40:40 +0100

    navbar ristorante tutta uguale
    uguale per tutte le pagine tranne la prima, che non ha torna indietro

--- ProvaConsegna/applicativo/frontend/ordini_ristorante.php
+++ ProvaConsegna/applicativo/frontend/ordini_ristorante.php
@@ -10,6 +10,7 @@
         <div class="navbar">
             <li><img src="../images/MainIcon.png" height="40px"></li>
                 <a href="ristorante.php">Torna indietro</a>
+                <a href="profilo_ristorante.php">Profilo</a>
                 <a href="modificaprofilo_ristorante.php">Modifica Profilo</a>
                 <a href="inserisci_piatto.html">Inserire nuovo piatto</a>
                 <a href="crea_menu.php">Crea menu</a>
