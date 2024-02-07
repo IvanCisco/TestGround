80bb712e4b8cc53d9d4ba21675cf36f71816346b
ilMarra <158060631+ilMarra@users.noreply.github.com>
2024-02-08 00:58:48 +0100

    Update Fattorino.php
    navbar

--- ProvaConsegna/applicativo/frontend/Fattorino.php
+++ ProvaConsegna/applicativo/frontend/Fattorino.php
@@ -11,36 +11,15 @@
 <body>
     
     
-    
+    <div class="header">
     <div class="navbar">
         <li><img src="../images/MainIcon.png" height="40px"></li>
-            <div class="menu-toggle" onclick="toggleMenu()">
-                <div class="bar"></div>
-                <div class="bar"></div>
-                <div class="bar"></div>
-            </div>
-            <div class="menu-items" id="menuItems">
-                <a href="profilo_fattorino.php">Profilo</a>
-                <a href="modificaprofilo_fattorino.php">Modifica Profilo</a>
-                <a href="ordini_acarico.php">Ordini a carico</a>
-                <a href="ordini_consegnati.php">Ordini consegnati</a>
-                <a href="../common/logout.php">Logout</a>
-            </div>
-            <div class="welcome-user">
-            
-            </div>
-            <div class="welcome-user">
-            
-            <?php
-                session_start();
-                if (isset($_SESSION['utente'])) {
-                    echo 'Benvenuto ' . $_SESSION['utente'];
-                    //echo 'il tuo tipo è ' . $_SESSION['tipo'] ;// Stampa il nome dell'utente
-                }
-            ?>
-        
-
-            </div>
+        <a href="profilo_fattorino.php">Profilo</a>
+        <a href="modificaprofilo_fattorino.php">Modifica Profilo</a>
+        <a href="ordini_acarico.php">Ordini a carico</a>
+        <a href="ordini_consegnati.php">Ordini consegnati</a>
+        <a href="../common/logout.php">Logout</a>
+        </div>
     </div>
 
     <h1>Ordini disponibili nella tua zona</h1>
@@ -55,7 +34,7 @@ mail rist si collega a location da cui posso prendere la zona e città
 */
 // connetto al database
 include("../common/connessione.php");
-//session_start();
+session_start();
 
 // se loggato
 if (isset($_SESSION['utente'])) {
