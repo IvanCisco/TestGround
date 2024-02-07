e84fcbd854ecea871af520f4940bce75ca392839
ilMarra <158060631+ilMarra@users.noreply.github.com>
2024-02-08 00:30:14 +0100

    Update ordini_ristorante.php
    mancava non so perchè

--- ProvaConsegna/applicativo/frontend/ordini_ristorante.php
+++ ProvaConsegna/applicativo/frontend/ordini_ristorante.php
@@ -11,6 +11,7 @@
             <li><img src="../images/MainIcon.png" height="40px"></li>
                 <a href="ristorante.php">Torna indietro</a>
                 <a href="modificaprofilo_ristorante.php">Modifica Profilo</a>
+                <a href="inserisci_piatto.html">Inserire nuovo piatto</a>
                 <a href="crea_menu.php">Crea menu</a>
                 <a href="ordini_ristorante.php">Ordini</a>
                 <a href="../common/logout.php">Logout</a>
