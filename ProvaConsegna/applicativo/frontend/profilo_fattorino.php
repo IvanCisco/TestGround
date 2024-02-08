db9761032c18558070e0a458d19abfbe8588135c
ilMarra <158060631+ilMarra@users.noreply.github.com>
2024-02-08 01:13:37 +0100

    Update profilo_fattorino.php

--- ProvaConsegna/applicativo/frontend/profilo_fattorino.php
+++ ProvaConsegna/applicativo/frontend/profilo_fattorino.php
@@ -17,7 +17,7 @@ $mail = $_SESSION['utente']; // Assumendo che l'email sia memorizzata in session
 <div class="header">
     <div class="navbar">
         <li><img src="../images/MainIcon.png" height="40px"></li>
-        <a href="Fattorino.php">Torna indietro</a></p>
+        <a href="Fattorino.php">Torna indietro</a>
         <a href="profilo_fattorino.php">Profilo</a>
         <a href="modificaprofilo_fattorino.php">Modifica Profilo</a>
         <a href="ordini_acarico.php">Ordini a carico</a>
