6cd58528d94e583f70547eca86faaf51b34bed81
ilMarra <158060631+ilMarra@users.noreply.github.com>
2024-02-09 15:13:44 +0100

    navbar fattorino

---
+++ ProvaConsegna/applicativo/common/navbar_fattorino.php
@@ -0,0 +1,11 @@
+<div class="header">
+    <div class="navbar">
+        <li><img src="../images/MainIcon.png" height="40px"></li>
+        <a href="Fattorino.php">Torna indietro</a>
+        <a href="profilo_fattorino.php">Profilo</a>
+        <a href="modificaprofilo_fattorino.php">Modifica Profilo</a>
+        <a href="ordini_acarico.php">Ordini a carico</a>
+        <a href="ordini_consegnati.php">Ordini consegnati</a>
+        <a href="../common/logout.php">Logout</a>
+        </div>
+    </div>
--- ProvaConsegna/applicativo/frontend/modificaprofilo_fattorino.php
+++ ProvaConsegna/applicativo/frontend/modificaprofilo_fattorino.php
@@ -20,17 +20,7 @@
 		<script src="../js/funzioni.js" async></script>
 		<link rel="stylesheet" type="text/css" href="../css/stile.css">
 	</head>
-	<div class="header">
-    <div class="navbar">
-        <li><img src="../images/MainIcon.png" height="40px"></li>
-        <a href="Fattorino.php">Torna indietro</a>
-        <a href="profilo_fattorino.php">Profilo</a>
-        <a href="modificaprofilo_fattorino.php">Modifica Profilo</a>
-        <a href="ordini_acarico.php">Ordini a carico</a>
-        <a href="ordini_consegnati.php">Ordini consegnati</a>
-        <a href="../common/logout.php">Logout</a>
-        </div>
-    </div>
+	<?php include("../common/navbar_fattorino.php"); ?>
 		<h1>Modifica Profilo</h1>
 <section class="page-content">
 	<body>
