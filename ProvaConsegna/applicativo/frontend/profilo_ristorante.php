4904176469908d2f0e08fd501a86e31baf7e3263
IvanCisco <munozivan76@yahoo.it>
2024-02-11 19:41:51 +0100

    PHP & CSS Changes
    - SAVED several lines of code in 'profilo_ristorante.php'.
    - CSS changes for 'profilo_ristorante.php'.
    - Shift table diplay in 'profilo_ristorante' not available yet!.

--- ProvaConsegna/applicativo/frontend/profilo_ristorante.php
+++ ProvaConsegna/applicativo/frontend/profilo_ristorante.php
@@ -5,147 +5,38 @@
         <link rel="stylesheet" type="text/css" href="../css/stile.css">
     </head>
     <body>
-        
-        <?php include("../common/navbar_ristorante.php"); ?>
+        <?php
+        include("../common/navbar_ristorante.php");
+        include("../backend/display_risto_info.php");
+        ?>
         <h1>Profilo Ristorante</h1>
         <section class="page-content">
-        <h3>Dati del ristorante </h3>
-
-        <?php
-        include '../common/connessione.php';
-        session_start();
-        // Recupero dei dati dell'utente
-        if(isset($_SESSION['utente'])) {
-            $mail = $_SESSION['utente']; // Assumendo che l'email sia memorizzata in sessione
-
-            //select dati del ristorante
-            $stmt = $conn->prepare("SELECT * FROM ristorante WHERE mail = ?");
-            $stmt->bind_param("s", $mail);
-            $stmt->execute();
-            $result = $stmt->get_result();
-
-            if ($result->num_rows > 0) {
-                while ($row = $result->fetch_assoc()) {
-        ?>
-                    <i class="far fa-edit edit-icon" onclick="editField('password')"></i>
-                    </p>
-                    <?php
-                    echo "<p>Password: " . $row["password"] . "</p>";
-                    echo "<p>Nome: " . $row["nome"] . "</p>";
-                    echo "<p>Ragione Sociale: " . $row["ragsoc"] . "</p>";
-                    echo "<p>PartitaIVA: " . $row["partitaiva"] . "</p>";
-                }
-            }
-                    ?>
-            <h3>Zona </h3>
-            <?php
-
-            //select per la zona in cui si trova il ristorante
-            $stmt = $conn->prepare("SELECT * FROM operainrist WHERE mailrist = ?");
-            $stmt->bind_param("s", $mail);
-            $stmt->execute();
-            $result = $stmt->get_result();
-
-            if ($result->num_rows > 0) {
-                while ($row = $result->fetch_assoc()) {
-                    echo "<p>Zona: " . $row["zona"] . "</p>";
-                }
-            }
-            ?>
-
-            <?php
-            //recupero id sedelegale e location
-            $stmt = $conn->prepare("SELECT location, sedelegale FROM ristorante WHERE mail = ?");
-            $stmt->bind_param("s", $mail);
-            $stmt->execute();
-            $result = $stmt->get_result();
-
-            $location = "";
-            $sedeLegale = "";
-
-            // Verifica se ci sono risultati
-            if ($result->num_rows > 0) {
-                // Estrai i dati e memorizzali nelle variabili
-                while ($row = $result->fetch_assoc()) {
-                    $location = $row["location"];
-                    $sedeLegale = $row["sedelegale"];
-                }
-            }
-            ?>
-
-            <h3>Sede Legale </h3>
-            <?php
-
-            //select per l'indirizzo della sede legale del ristorante
-            $stmt = $conn->prepare("SELECT * FROM indirizzo WHERE id = ?");
-            $stmt->bind_param("s", $sedeLegale);
-            $stmt->execute();
-            $result = $stmt->get_result();
-
-
-            if ($result->num_rows > 0) {
-                while ($row = $result->fetch_assoc()) {
-            ?>
-                    <i class="far fa-edit edit-icon" onclick="editField('password')"></i>
-                    </p>
-                    <?php
-                    echo "<p>Via: " . $row["via"] . "</p>";
-                    echo "<p>Numero: " . $row["numero"] . "</p>";
-                    echo "<p>Cap: " . $row["cap"] . "</p>";
-                    echo "<p>Città: " . $row["citta"] . "</p>";
-                }
-            }
-                    ?>
-            <h3>Location</h3>
-            <?php
-
-            //select per l'indirizzo in cui si trova il ristorante
-            $stmt = $conn->prepare("SELECT * FROM indirizzo WHERE id = ?");
-            $stmt->bind_param("s", $location);
-            $stmt->execute();
-            $result = $stmt->get_result();
-
-
-            if ($result->num_rows > 0) {
-                while ($row = $result->fetch_assoc()) {
-            ?>
-                    <i class="far fa-edit edit-icon" onclick="editField('password')"></i>
-                    </p>
-                    <?php
-                    echo "<p>Via: " . $row["via"] . "</p>";
-                    echo "<p>Numero: " . $row["numero"] . "</p>";
-                    echo "<p>Cap: " . $row["cap"] . "</p>";
-                    echo "<p>Città: " . $row["citta"] . "</p>";
-                }
-            }
-
-        } else {
-            echo "Utente non loggato";
-        }
-                    ?>
-
-        <h3>I miei orari</h3>
-        <?php
-        //select turni del ristorante
-        $stmt = $conn->prepare("SELECT * FROM rlavorasu WHERE mailrist = ?");
-        $stmt->bind_param("s", $mail);
-        $stmt->execute();
-        $result = $stmt->get_result();
-
-
-
-        if ($result->num_rows > 0) {
-            while ($row = $result->fetch_assoc()) {
-        
-                echo "<p>Giorno: " . $row["giorno"] . "</p>";
-                echo "<p>inizio turno: " . $row["orainizio"] . "</p>";
-                echo "<p>fine turno: " . $row["orafine"] . "</p>";
-            }
-        }else {
-            echo "Non è stato trovato alcun turno, recarsi nella sezione modifica profilo per aggiungerne";
-        }
-        ?>
-    </section>
+            <div class=wrapperInfoRist>
+                <div class="infoRist">
+                    <h2>Dati del ristorante</h2>
+                    <p>Nome: <?php echo $row['nome'];?></p>
+                    <p>Ragione sociale: <?php echo $row['ragsoc'];?></p>
+                    <p>Partita IVA: <?php echo $row['partitaiva'];?></p>
+                    <p>E-mail: <?php echo $row['mail'];?></p>
+                    <p>Password: <?php echo $row['password'];?></p>
+                </div>
+                <div class="infoRist">
+                    <h2>Location</h2>
+                    <p><?php echo $row['via'] . " " . $row['numero'] . ",";?></p>
+                    <p><?php echo $row['cap'] . ",";?>
+                    <p><?php echo $row['citta'] . ", Zona " . $row['zona'];?></p>
+                </div>
+                <div class="infoRist">
+                    <h2>Sede legale</h2>
+                    <p><?php echo $row['viasl'] . " " . $row['numerosl'];?></p>
+                    <p><?php echo $row['capsl'];?>
+                    <p><?php echo $row['cittasl'];?></p>
+                </div>
+            </div>
+            <div class="orariRisto">
+                <h2>Orari di esercizio</h2>
+            </div>
+        </section>
     </body>
     <?php include("../common/footer.html"); ?>
 </html>
