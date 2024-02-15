9d328d3df6fd72858af12a80235eaf68cac0336a
IvanCisco <munozivan76@yahoo.it>
2024-02-15 22:59:20 +0100

    RIDER'S SCHEDULE DELETION
    Rider's schedule deletion now works properly

--- ProvaConsegna/applicativo/backend/visualizza_orari2.php
+++ ProvaConsegna/applicativo/backend/visualizza_orari2.php
@@ -9,7 +9,7 @@ $risultato = $conn->query($sql);
 if ($risultato->num_rows > 0) {
     while($row = $risultato->fetch_assoc()) {
         echo "<p class=\"" . $row['giorno'] . $row['orainizio'] . $row['orafine'] . "\">" . $row['giorno'] . "   " . $row['orainizio'] . " - " . $row['orafine'] . " ";
-        echo "<img src='../immagini/delete-icon2.png' onclick=\"eliminaOrari('{$row['giorno']}','{$row['orainizio']}', '{$row['orafine']}', '{$mail}', 'rlavorasu')\"></img></p>";
+        echo "<img src='../immagini/delete-icon2.png' onclick=\"eliminaOrari('{$row['giorno']}','{$row['orainizio']}', '{$row['orafine']}', '{$mail}', '{$tabella}')\"></img></p>";
     }
 } else {
     echo "<p>Non ci sono orari da mostrare.</p>";
