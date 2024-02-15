4fbf0078441f14d3e21eafcf0b1caeac679477db
IvanCisco <munozivan76@yahoo.it>
2024-02-15 22:44:35 +0100

    Minor Fix
    '/backend/display_risto_info.php', line 12:
    
    operainrist.mailfatt---->operainrist.mail

--- ProvaConsegna/applicativo/backend/display_risto_info.php
+++ ProvaConsegna/applicativo/backend/display_risto_info.php
@@ -9,7 +9,7 @@ if (!isset($_SESSION["utente"])) {
     $sql = "SELECT ristorante.*, indirizzo.*, sedelegale.via AS viasl, sedelegale.numero AS numerosl, sedelegale.cap AS capsl, sedelegale.citta AS cittasl, operainrist.zona
             FROM ristorante INNER JOIN indirizzo ON ristorante.location = indirizzo.id
             LEFT JOIN indirizzo AS sedelegale ON ristorante.sedelegale = sedelegale.id
-            LEFT JOIN operainrist ON ristorante.mail = operainrist.mailrist
+            LEFT JOIN operainrist ON ristorante.mail = operainrist.mail
             WHERE ristorante.mail = '$mail';";
     $risultato = $conn->query($sql);
     if ($risultato->num_rows > 0) {
