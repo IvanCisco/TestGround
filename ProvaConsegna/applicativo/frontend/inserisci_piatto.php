d77d79810d4b308882e6c35e29cc08f589718370
IvanCisco <munozivan76@yahoo.it>
2024-02-15 20:02:53 +0100

    Minor Fix
    inserisci_piatto.php, line 14:
    
    inserisci_piatto.html ---> inserisci_piatto.php

--- ProvaConsegna/applicativo/frontend/inserisci_piatto.php
+++ ProvaConsegna/applicativo/frontend/inserisci_piatto.php
@@ -11,7 +11,7 @@
                 <a href="ristorante.php">Torna indietro</a>
                 <a href="profilo_ristorante.php">Profilo</a>
                 <a href="modificaprofilo_ristorante.php">Modifica Profilo</a>
-                <a href="inserisci_piatto.html">Inserire nuovo piatto</a>
+                <a href="inserisci_piatto.php">Inserire nuovo piatto</a>
                 <a href="crea_menu.php">Crea menu</a>
                 <a href="ordini_ristorante.php">Ordini</a>
                 <a href="../common/logout.php">Logout</a>
